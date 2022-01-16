d1428ca86b4a74b040f675b6a995b0474e2234e2
phaptrinh <phap.n.trinh00@gmail.com>
2022-01-16 11:58:19 +0700

    create project

---
+++ src/main/java/com/example/socialeventwebsite/SocialEventWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialeventwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialEventWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialEventWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/socialeventwebsite/SocialEventWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialeventwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialEventWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
